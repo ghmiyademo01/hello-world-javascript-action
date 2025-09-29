0eb4d82904c8be1db657da864fc7b05b48505175
GHubDemoMiya <kemiyamoto@kemiya365.onmicrosoft.com>
2025-09-30 01:34:02 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import * as core from "@actions/core"; // GitHub Actions の入力・出力・ログ・エラー処理などを扱うライブラリ。
 import * as github from "@actions/github"; // GitHub のイベントコンテキスト（トリガーされたイベントの詳細）を取得するためのライブラリ。
 
+import http from 'http';
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('OK\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet"); // Action.yml で定義された入力値 "who-to-greet" を取得。
